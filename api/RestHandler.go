090ab4e89d2172fed932f63b9915c66da61d85e8
Vikram Singh <vikram@devtron.ai>
2021-07-19 13:01:09 +0530

    removed unused code

--- api/RestHandler.go
+++ api/RestHandler.go
@@ -89,10 +89,3 @@ func (impl *RestHandlerImpl) CheckForOptOut(w http.ResponseWriter, r *http.Reque
 	}
 	impl.writeJsonResp(w, err, isOptOut, 200)
 }
-
-func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	(*w).Header().Set("Content-Type", "text/html; charset=utf-8")
-}
